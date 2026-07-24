928cbb7e35002bca94775bb1478310e12d64b6f4
Vitalik0422 <vitaliy19980422@gmail.com>
2026-07-24 16:01:45 +0300

    feat: initialize Node.js Express application with basic server setup
    - Added package.json with dependencies and scripts for starting the server
    - Created src/server.js with Express setup, including routes and error handling
    - Integrated pino-http for logging and cors for cross-origin requests

---
+++ src/server.js
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+import express from 'express';
+import logger from 'pino-http';
+import cors from 'cors';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  logger({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  req.log.info('something else');
+  res.send('hello world');
+});
+app.get('/notes', (req, res) => {
+  res.json({
+    message: 'Retrieved all notes',
+  });
+});
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Something went wrong');
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is start on ${PORT}`);
+});
