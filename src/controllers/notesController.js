7f348c74e84e3a4e2c4366205451edfe52cea677
Vitalik0422 <vitaliy19980422@gmail.com>
2026-07-25 23:05:04 +0300

    refactor: simplify response structure for create, delete, and update note operations

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -32,14 +32,14 @@ export const getNoteById = async (req, res) => {
 export const createNote = async (req, res) => {
   const note = req.body;
   const createdNote = await noteModel.create(note);
-  res.status(201).json({ message: 'The note was created', note: createdNote });
+  res.status(201).json({ note: createdNote });
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await noteModel.findByIdAndDelete(noteId);
   if (!note) throw createHttpError(404, 'The note not found');
-  res.status(200).json({ message: 'The note was deleted', note });
+  res.status(200).json({ note });
 };
 
 export const updateNote = async (req, res) => {
@@ -50,5 +50,5 @@ export const updateNote = async (req, res) => {
     runValidators: true,
   });
   if (!updatedNote) throw createHttpError(404, 'The note not found');
-  res.status(200).json({ message: 'The note was updated', note: updatedNote });
+  res.status(200).json({ note: updatedNote });
 };
