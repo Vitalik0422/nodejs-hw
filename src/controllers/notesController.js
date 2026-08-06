b7cfbd3a93ed92cffe22e7bce0b4eccc51d8857d
Віталій <vitaliy19980422@gmail.com>
2026-08-06 19:35:01 +0300

    fix: simplify response structure for note retrieval and creation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -26,20 +26,20 @@ export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
   if (!note) throw createHttpError(404, 'The note not found');
-  res.status(200).json({ note });
+  res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
   const note = req.body;
   const createdNote = await Note.create(note);
-  res.status(201).json({ createdNote });
+  res.status(201).json(createdNote);
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findByIdAndDelete(noteId);
   if (!note) throw createHttpError(404, 'The note not found');
-  res.status(200).json({ note });
+  res.status(200).json(note);
 };
 
 export const updateNote = async (req, res) => {
@@ -50,5 +50,5 @@ export const updateNote = async (req, res) => {
     runValidators: true,
   });
   if (!updatedNote) throw createHttpError(404, 'The note not found');
-  res.status(200).json({ updatedNote });
+  res.status(200).json(updatedNote);
 };
