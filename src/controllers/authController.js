3477fe04e7e3eceeb4a69c14b5083e10420298b6
Віталій <vitaliy19980422@gmail.com>
2026-08-16 00:03:55 +0300

    fix:  refresh endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -34,7 +34,8 @@ export const loginUser = async (req, res) => {
 
 export const refreshUserSession = async (req, res) => {
   const { sessionId, refreshToken } = req.cookies;
-  const session = await Session.findOne({ sessionId, refreshToken });
+  console.log(sessionId, refreshToken);
+  const session = await Session.findOne({ _id: sessionId, refreshToken });
 
   if (!session) throw createHttpError(401, 'Session not found');
 
